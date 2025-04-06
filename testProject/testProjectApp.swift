0ded48694e4e21476446b6cdc8638742f6788e21
Mateus <mateus@MacBook-Air-de-Fix.local>
2025-04-06 09:50:58 -0300

    feat: Project for study

---
+++ testProject/testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectApp.swift
+//  testProject
+//
+//  Created by Mateus on 06/04/25.
+//
+
+import SwiftUI
+
+@main
+struct testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
